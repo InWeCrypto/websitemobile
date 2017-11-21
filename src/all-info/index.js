60aad85a3a331fe7394e1b37b453290c2e999f80
杨叙 <yangxu@yangxudeMacBook-Pro.local>
2017-11-21 20:59:49 +0800

    111

--- src/all-info/index.js
+++ src/all-info/index.js
@@ -26,7 +26,20 @@ export default class AppComponent extends Component {
 					<div className="list-box">
 						<div className="group">
 							<div className="group-title">222</div>
-							<div className="group-cont">22</div>
+							<div className="group-cont">
+								<div className="img-box">
+									<img src="https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3065766055,342841074&fm=173&s=7502D31C4225051538207F810300F08E&w=218&h=146&img.JPG" />
+								</div>
+								<div className="group-info">1</div>
+							</div>
+							<div className="group-ctrl">
+								<span className="txt">
+									2017-09-09 11:11:11 &nbsp;|&nbsp;
+								</span>
+								<span className="txt">阅读：435&nbsp;|&nbsp;</span>
+								<span className="txt">回复：435&nbsp;|&nbsp;</span>
+								<span className="txt">收藏</span>
+							</div>
 						</div>
 					</div>
 				</div>
