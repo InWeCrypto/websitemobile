d0219c662151d15634c255e72268c93db206faa2
杨叙 <yangxu@yangxudeMacBook-Pro.local>
2017-11-26 16:12:40 +0800

    111

--- src/views/index/components/project/index.js
+++ src/views/index/components/project/index.js
@@ -19,7 +19,7 @@ class ChainList {
 	}
 	findByType(id, type) {
 		if (this._this[id].type != type) {
-			this.find(this._this[id].next, type);
+			return this.findByType(this._this[id].next, type);
 		} else {
 			return this._this[id];
 		}
@@ -28,8 +28,13 @@ class ChainList {
 		let item = this._this[id];
 		let prev = this._this[this._this[id].prev];
 		let next = this._this[this._this[id].next];
-		prev.next = next.data.id;
-		next.prev = prev.data.id;
+		if (prev) {
+			prev.next = this._this[id].next;
+		}
+		if (next) {
+			next.prev = this._this[id].prev;
+		}
+
 		delete this._this[id];
 		this.length--;
 	}
@@ -60,7 +65,33 @@ export default class project extends Component {
 				type: item.grid_type
 			});
 		});
-		console.log(list.getLength());
+		let list1 = list.getList();
+		let keys = Object.keys(list1);
+		var newList = [];
+
+		data.map((item, index) => {
+			if (!list1[item.id]) {
+				return;
+			}
+			if (item.grid_type == 1) {
+				newList.push(item);
+				let id = item.id;
+				if (list1[id].next) {
+					let i2 = list.findByType(list1[id].next, 1);
+					if (i2) {
+						newList.push(i2.data);
+						list.remove(i2.id);
+					}
+				}
+				list.remove(item.id);
+			} else {
+				newList.push(item);
+				list.remove(item.id);
+			}
+		});
+		newList.map(item => {
+			console.log(item.grid_type);
+		});
 	}
 	render() {
 		const { projectList } = this.props;
